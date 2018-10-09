76784460af338ae76fca0cb950c6f4a05bdc435f
nisted <clement.p10@laposte.net>
2018-10-09 10:40:36 +0200

    premier depot

---
+++ script.js
@@ -0,0 +1,4 @@
+// déclaration de la variable name initialiser avec une chaine de caractère (string)
+var name = "blabla";
+// affichage de ma variable name dans une boite de dialogue que j'appelle avec la fonction alerte
+  alert(name);
